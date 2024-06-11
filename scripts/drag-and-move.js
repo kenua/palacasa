5adadf8ffa09d412d56fd928cd0d915ecbbf9051
Kenua <juanestebanmedinag@gmail.com>
2024-06-10 19:52:34 -0500

    Rename script file

--- scripts/drag-and-move.js
+++ scripts/drag-and-move.js
@@ -3,30 +3,30 @@
 window.addEventListener('DOMContentLoaded', () => {
   const collectionsContainer = document.getElementById('collections-container');
 
-  let isGrabbing = false;
+  let isDragging = false;
   let initialPos = null;
   let currentScroll = 0
 
   collectionsContainer.scrollLeft = 0;
-  collectionsContainer.addEventListener('mousedown', grabSetup);
+  collectionsContainer.addEventListener('mousedown', dragSetup);
   collectionsContainer.addEventListener('mousemove', moveScroll);
   collectionsContainer.addEventListener('mouseup', drop);
   collectionsContainer.addEventListener('mouseleave', drop);
 
-  function grabSetup(e) {
+  function dragSetup(e) {
     e.preventDefault();
     initialPos = e.clientX;
-    isGrabbing = true;
+    isDragging = true;
   }
 
   function moveScroll(e) {
-    if (isGrabbing) {
+    if (isDragging) {
       collectionsContainer.scrollLeft = currentScroll + (initialPos - e.clientX);
     }
   }
 
   function drop() {
-    isGrabbing = false;
+    isDragging = false;
     currentScroll = collectionsContainer.scrollLeft;
   }
 });
