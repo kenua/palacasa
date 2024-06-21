93a81dbae4aba8df91d4fa017d91c8f778167762
Kenua <juanestebanmedinag@gmail.com>
2024-06-20 22:06:27 -0500

    Add scroll buttons

--- scripts/drag-and-move.js
+++ scripts/drag-and-move.js
@@ -2,16 +2,25 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   const collectionsContainer = document.getElementById('collections-container');
+  const prevButton = document.getElementById('prev');
+  const nextButton = document.getElementById('next');
 
+  let isDown = false;
   let isDragging = false;
   let initialPos = null;
   let currentScroll = 0
 
+  // drag to scroll 'collectionsContainer'
   collectionsContainer.scrollLeft = 0;
   collectionsContainer.addEventListener('mousedown', dragSetup);
   collectionsContainer.addEventListener('mousemove', moveScroll);
   collectionsContainer.addEventListener('mouseup', drop);
   collectionsContainer.addEventListener('mouseleave', drop);
+  // scroll left or right using buttons
+  prevButton.addEventListener('mousedown', () => scrollCollection(-75))
+  prevButton.addEventListener('mouseup', () => isDown = false)
+  nextButton.addEventListener('mousedown', () => scrollCollection(75))
+  nextButton.addEventListener('mouseup', () => isDown = false)
 
   function dragSetup(e) {
     e.preventDefault();
@@ -28,5 +37,21 @@ window.addEventListener('DOMContentLoaded', () => {
   function drop() {
     isDragging = false;
     currentScroll = collectionsContainer.scrollLeft;
+  }
+
+  function scrollCollection(scrollValue) {
+    isDown = true;
+    moveCollection(scrollValue)
+  }
+
+  function moveCollection(scrollValue) {
+    if (isDown) {
+      collectionsContainer.scrollBy({
+        left: scrollValue,
+        behavior: 'smooth',
+      });
+      currentScroll = collectionsContainer.scrollLeft;
+      requestAnimationFrame(() => moveCollection(scrollValue));
+    }
   }
 });
